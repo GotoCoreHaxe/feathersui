b1fb7fb932ecc34840af5ee44dd9c4a27d7b21b3
Gábor Váradi <varadi83gabor@gmail.com>
2019-12-29 16:45:25 +0100

    .

---
+++ src/feathers/controls/GuiLayoutGroup.hx
@@ -0,0 +1,11 @@
+package feathersui.src.feathers.controls;
+
+import feathers.controls.LayoutGroup;
+
+@:build(core.utils.CoreClassMacro.buildFields("gui.layout.group"))
+class GuiLayoutGroup extends LayoutGroup {
+	public function new() {
+		super();
+		core.utils.CoreClassMacro.construct();
+	}
+}
